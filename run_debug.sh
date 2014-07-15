3e555853370b9fde30dc04f6e2e5f7f8d7a3a0eb
Jiří Kuneš <jirka642@gmail.com>
2014-07-15 21:46:37 +0200

    run_debug -> x-terminal-emulator

--- run_debug.sh
+++ run_debug.sh
@@ -2,5 +2,5 @@
 if [ "$1" = "-t" ]; then
     python src/main.py -d
 else
-    gnome-terminal -e 'bash -c "./run_debug.sh -t ; bash"'
+    x-terminal-emulator -e 'bash -c "./run_debug.sh -t ; bash"'
 fi
